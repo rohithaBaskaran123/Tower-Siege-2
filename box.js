6949e5468e1395128992f27dab95bd739afe8db2
rohithaBaskaran123 <70988626+rohithaBaskaran123@users.noreply.github.com>
2021-01-06 18:28:13 -0800

    Update box.js

--- box.js
+++ box.js
@@ -1,28 +1,23 @@
 class Box {
     constructor(x,y,width,height) {
-      super(x,y,50,50);
+     
       var options = {
           isStatic: false,
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      World.add(world, this.baody);
+      World.add(world, this.body);
     }
     display(){
       var ps =this.body.position;
       rectMode(CENTER);
-      rect(ps.x, ps.y, this.width, this.height);
+      
       if(this.body.speed < 3){
-        super.display();
+        rect(ps.x, ps.y, this.width, this.height);  
        }
        else{
         World.remove(world, this.body);
-        push();
-        this.Visiblity = this.Visiblity - 5;
-        tint(255,this.Visiblity);
-        image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-        pop();
       }
     }
    
